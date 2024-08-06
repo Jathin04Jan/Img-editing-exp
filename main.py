1d4249e782a91a0d056acbe988ed6161df067c55
Jathin04Jan <97787089+Jathin04Jan@users.noreply.github.com>
2024-08-06 22:09:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -80,4 +80,4 @@ def download_all():
 
 #app.run()
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
